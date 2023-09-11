ffe14ced381d82cf0b46c8a1bba176f867cbbf71
Carlos <carlosmabraham@gmail.com>
2023-09-11 09:23:46 -0700

    v-on v-for

--- js/app.js
+++ js/app.js
@@ -5,10 +5,28 @@ createApp({
         const name = ref('')
         const count = ref(0)
         const resultado = ref(0)
+        const userName = ref('')
+        const lastName = ref('')
+        const persons = ref([])
         return {
             name,
             count,
-            resultado
+            resultado,
+            userName,
+            lastName, 
+            persons
+        }
+    },
+    methods:{
+        addPerson(){
+            const person = {
+                userName:this.userName, 
+                lastName:this.lastName
+            }
+            // console.log('Hola Vue')
+            // console.log(this.name)
+            // console.log(this.lastName)
+            this.persons.push(person)
         }
     }
 }).mount('#app')
